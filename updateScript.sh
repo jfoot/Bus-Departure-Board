d3b3944a701bb8b9a8c1b31673f3878624a3d95f
Jonathan Foot <jonathanfoot@hotmail.co.uk>
2020-12-19 14:04:24 +0000

    Update updateScript.sh

--- updateScript.sh
+++ updateScript.sh
@@ -9,9 +9,9 @@ if [ $LOCAL = $REMOTE ]; then
     echo "Program is already up-to date."
 elif [ $LOCAL = $BASE ]; then
     echo "Program Update in progress..."
-	sudo git pull
-	echo "Device restarting now."
-	sudo restart now
+    sudo git pull
+    echo "Device restarting now."
+    sudo restart now
 elif [ $REMOTE = $BASE ]; then
     echo "You've made local changes which is stopping the ability to update your device. Please use 'git stash' if your unsure on what to do."
 else
